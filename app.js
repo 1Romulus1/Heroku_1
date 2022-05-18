0b2a65ff684916b654346646da80af428ef7441d
Roman <kyvaroooman@gmail.com>
2022-05-18 16:39:20 +0300

    deploy#6

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
-import express from 'express';
-import mongoose from 'mongoose';
-// import cors from 'cors';
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import "dotenv/config";
 // import path from 'path'
-import collectionsRouter from './routes/collectionsRoutes.js'
-import usersRourer from './routes/usersRoutes.js';
+import collectionsRouter from "./routes/collectionsRoutes.js";
+import usersRourer from "./routes/usersRoutes.js";
 
 const port = process.env.PORT || 5000;
 // const MONGO_URI = "mongodb+srv://Roman:jeMICw6PoSLtDlVe@cluster4.ofmce.mongodb.net/real_undefined?retryWrites=true&w=majority";
 
 const app = express();
 
-// app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/collections", collectionsRouter);
 app.use("/api/users", usersRourer);
